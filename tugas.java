487cd50c9c4eafc6cda749b3f656f7e7af1d3db3
FakhriZuhdi <149861928+FakhriZuhdi@users.noreply.github.com>
2023-11-04 13:34:53 +0700

    Add files via upload

---
+++ tugas.java
@@ -0,0 +1,19 @@
+public class tugas {
+
+    public static void main (String[] args){
+        String nama = "Fakhri Zuhdi Alfarros";
+        String NPM = "G1A023050";
+        System.out.println("Nama : " + nama) ;
+        System.out.println("NPM : " + NPM);
+        System.out.println("==================") ;
+        for (int i = 0 ; i<= 100 ; i++){
+            if (i>= 10){
+                System.out.println(nama);
+            }
+            else{
+                System.out.println(i);
+            }
+        }
+
+    }
+}
